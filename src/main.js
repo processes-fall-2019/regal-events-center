d0dc4cce35d7b7394aa90c48c1155ad9c1470ce1
Francisco Franco <fhfranco32@gmail.com>
2019-10-24 13:37:42 -0400

    added some bootstrap

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+import 'bootstrap-css-only/css/bootstrap.min.css'
+import 'mdbvue/build/css/mdb.css'
+
 Vue.use(BootstrapVue)
 
 Vue.config.productionTip = false
