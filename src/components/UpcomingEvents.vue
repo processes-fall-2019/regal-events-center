7edb24556272bf586fee2f1143ad1dcad811a09b
Francisco Franco <fhfranco32@gmail.com>
2019-10-24 16:59:02 -0400

    created past and upcoming events pages

---
+++ src/components/UpcomingEvents.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <NavigationBar></NavigationBar>
+    <h1> Upcoming Events </h1>
+  </div>
+</template>
+
+<script>
+import NavigationBar from './NavigationBar'
+
+export default {
+  name: 'UpcomingEvents',
+  props: {
+    msg: String
+  },
+  components: {
+    NavigationBar
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
