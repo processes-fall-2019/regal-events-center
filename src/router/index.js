e8f5e71a466f21c5df6fb05e76163cd04c533da3
Francisco Franco <fhfranco32@gmail.com>
2019-10-24 16:22:51 -0400

    added gallery route

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
 import AboutUs from '@/components/AboutUs'
+import Gallery from '@/components/Gallery'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/aboutus',
       name: 'aboutus',
       component: AboutUs
+    },
+    {
+      path: '/gallery',
+      name: 'gallery',
+      component: Gallery
     },
   ]
 })
