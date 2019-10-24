0f182a7f726e7e8b93bbd0c65613eb0c288f905a
Francisco Franco <fhfranco32@gmail.com>
2019-10-24 02:44:24 -0400

    added db connection to server

--- server.js
+++ server.js
@@ -7,6 +7,28 @@ const path = require('path')
 const port = process.env.PORT || 8080
 const app = express()
 
+// establish connection with database using Knex ORM
+var knex = require('knex')({
+  client: 'pg',
+  connection: {
+    host: '127.0.0.1',
+    port: '5437',
+    user: 'postgres',
+    password: '',
+    database: 'contact-manager-database'
+  }
+})
+
+// // for production database
+// var knex = require('knex')({
+//   client: 'pg',
+//   connection: 'postgres://foebrtpffobifj:014530deb0ab7d7a01ec5e7392a4f6b44c645e6a31bef640c10312980e2bae24@ec2-107-20-243-220.compute-1.amazonaws.com:5432/d6o9t4b4gd3lni',
+//   migrations: {
+//     tableName: 'knex_migrations'
+//   },
+//   ssl: true
+// })
+
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 
