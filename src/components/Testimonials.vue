b73998e89f3e225dac7bec439ae59333eb6b7611
Francisco Franco <fhfranco32@gmail.com>
2019-10-24 16:59:14 -0400

    created testimonials page

---
+++ src/components/Testimonials.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <NavigationBar></NavigationBar>
+    <h1> Testimonials </h1>
+  </div>
+</template>
+
+<script>
+import NavigationBar from './NavigationBar'
+
+export default {
+  name: 'Testimonials',
+  props: {
+    msg: String
+  },
+  components: {
+    NavigationBar
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
