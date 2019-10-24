e9c821a6b9b1c66690bdcf2ca59ff5412fe650d6
Francisco Franco <fhfranco32@gmail.com>
2019-10-24 16:58:34 -0400

    created grace page

---
+++ src/components/Grace.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <NavigationBar></NavigationBar>
+    <h1> Grace </h1>
+  </div>
+</template>
+
+<script>
+import NavigationBar from './NavigationBar'
+export default {
+  name: 'Grace',
+  props: {
+    msg: String
+  },
+  components: {
+    NavigationBar
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
