95462cfbe0315e2d9f324e2fe4a9710a8a2b9454
Francisco Franco <fhfranco32@gmail.com>
2019-10-24 16:58:02 -0400

    created book venue page

---
+++ src/components/BookVenue.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <NavigationBar></NavigationBar>
+    <h1> Book Venue </h1>
+  </div>
+</template>
+
+<script>
+import NavigationBar from './NavigationBar'
+
+export default {
+  name: 'BookVenue',
+  props: {
+    msg: String
+  },
+  components: {
+    NavigationBar
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
