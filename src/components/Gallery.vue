d787aabd41a7038fdac4ac4db02c0dd0299fa350
Francisco Franco <fhfranco32@gmail.com>
2019-10-25 23:08:11 -0400

    gallery update

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -43,6 +43,16 @@ import NavigationBar from './NavigationBar'
          {
            src: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(98).jpg',
          },
+         {
+           src: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(98).jpg',
+         },
+         {
+           src: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(117).jpg',
+           caption: 'Image 1',
+         },
+         {
+           src: 'https://mdbootstrap.com/img/Photos/Horizontal/Nature/12-col/img%20(98).jpg',
+         },
        ],
        visible: false,
        index: 0
