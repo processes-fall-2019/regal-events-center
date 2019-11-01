d5df19b227e61801e41e85e6adb965ff805f5679
Francisco Franco <fhfranco32@gmail.com>
2019-11-01 02:46:32 -0400

    moved db connection to own file

--- server.js
+++ server.js
@@ -6,28 +6,7 @@ const morgan = require('morgan')
 const path = require('path')
 const port = process.env.PORT || 8080
 const app = express()
-
-// establish connection with database using Knex ORM
-var knex = require('knex')({
-  client: 'pg',
-  connection: {
-    host: '127.0.0.1',
-    port: '5437',
-    user: 'postgres',
-    password: '',
-    database: 'contact-manager-database'
-  }
-})
-
-// // for production database
-// var knex = require('knex')({
-//   client: 'pg',
-//   connection: 'postgres://foebrtpffobifj:014530deb0ab7d7a01ec5e7392a4f6b44c645e6a31bef640c10312980e2bae24@ec2-107-20-243-220.compute-1.amazonaws.com:5432/d6o9t4b4gd3lni',
-//   migrations: {
-//     tableName: 'knex_migrations'
-//   },
-//   ssl: true
-// })
+const knex = require('./db/db')
 
 app.use(morgan('combined'))
 app.use(bodyParser.json())
@@ -36,7 +15,7 @@ app.use(bodyParser.json())
 // app.use(serveStatic(path.join(__dirname, '/dist')))
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
-// require('./routes')(app, knex)
+require('./routes')(app, knex)
 
 app.listen(port)
 
