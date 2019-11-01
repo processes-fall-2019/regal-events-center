c5e011eafb30481a95f3bb1db3107ede74eeb80d
Francisco Franco <fhfranco32@gmail.com>
2019-11-01 12:53:55 -0400

    added mobile scalability to home, footer, and nav bar

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,70 +1,75 @@
 <template>
   <div class="hello">
+    <head>
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width">
+    </head>
     <!-- Footer -->
-  <mdb-footer color="stylish-color-dark" class="page-footer font-medium pt-4 mt-4">
-    <mdb-container class="text-left">
-      <mdb-row>
-        <mdb-col md="6">
-          <h5 class="text-uppercase mb-4 mt-3 font-weight-bold">Footer Content</h5>
-          <p>Here you can use rows and columns here to organize your footer content. Lorem ipsum dolor sit
-            amet, consectetur adipisicing elit.</p>
-        </mdb-col>
-        <hr class="clearfix w-100 d-md-none" />
-        <mdb-col md="2">
-          <h5 class="text-uppercase mb-4 mt-3 font-weight-bold">Links</h5>
-          <ul class="list-unstyled">
-            <li><a href="#/">Home</a></li>
-            <li><a href="#/grace">Grace</a></li>
-            <li><a href="#/testimonials">Testimonials</a></li>
-            <li><a href="#/gallery">Gallery</a></li>
-            <li><a href="#/bulletin">Bulletin</a></li>
-          </ul>
-        </mdb-col>
-        <hr class="clearfix w-100 d-md-none" />
-        <mdb-col md="2">
-          <h5 class="text-uppercase mb-4 mt-3 font-weight-bold">Links</h5>
-          <ul class="list-unstyled">
-            <li><a href="#/upcomingevents">Upcoming Events</a></li>
-            <li><a href="#/pastevents">Past Events</a></li>
-            <li><a href="#/venuelayout">Layout & Pricing</a></li>
-            <li><a href="#/bookvenue">Book Venue</a></li>
-          </ul>
-        </mdb-col>
-        <hr class="clearfix w-100 d-md-none" />
-        <mdb-col md="2">
-          <h5 class="text-uppercase mb-4 mt-3 font-weight-bold">Links</h5>
-          <ul class="list-unstyled">
-            <li><a href="#/admin">Admin Login</a></li>
-          </ul>
-        </mdb-col>
-      </mdb-row>
-    </mdb-container>
-    <br />
-    <hr />
-    <div class="text-center py-3">
-      <ul class="list-unstyled list-inline mb-0">
-        <li class="list-inline-item">
-          <h5 class="mb-1">Reserve an event today</h5>
-        </li>
-        <li class="list-inline-item"><a href="#/bookvenue" class="btn btn-primary btn-rounded">Book an event!</a></li>
-      </ul>
-    </div>
-    <hr />
-    <div class="text-center">
-      <ul class="list-unstyled list-inline">
-        <br />
-        <li class="list-inline-item"><a class="btn-floating btn-lg btn-fb mx-1"><i class="fab fa-facebook"> </i></a></li>
-        <li class="list-inline-item"><a class="btn-floating btn-lg btn-tw mx-1"><i class="fab fa-twitter"> </i></a></li>
-      </ul>
-    </div>
-    <br />
-    <div class="footer-copyright text-center py-3">
-      <mdb-container fluid>
-        &copy; 2019 Copyright: <a href="#/"> Regal Events Center </a>
+    <mdb-footer color="stylish-color-dark" class="page-footer font-medium pt-4 mt-4">
+      <mdb-container class="text-left">
+        <mdb-row>
+          <mdb-col md="6">
+            <h5 class="text-uppercase mb-4 mt-3 font-weight-bold">Footer Content</h5>
+            <p>Here you can use rows and columns here to organize your footer content. Lorem ipsum dolor sit
+              amet, consectetur adipisicing elit.</p>
+          </mdb-col>
+          <hr class="clearfix w-100 d-md-none" />
+          <mdb-col md="2">
+            <h5 class="text-uppercase mb-4 mt-3 font-weight-bold">Links</h5>
+            <ul class="list-unstyled">
+              <li><a href="#/">Home</a></li>
+              <li><a href="#/grace">Grace</a></li>
+              <li><a href="#/testimonials">Testimonials</a></li>
+              <li><a href="#/gallery">Gallery</a></li>
+              <li><a href="#/bulletin">Bulletin</a></li>
+            </ul>
+          </mdb-col>
+          <hr class="clearfix w-100 d-md-none" />
+          <mdb-col md="2">
+            <h5 class="text-uppercase mb-4 mt-3 font-weight-bold">Links</h5>
+            <ul class="list-unstyled">
+              <li><a href="#/upcomingevents">Upcoming Events</a></li>
+              <li><a href="#/pastevents">Past Events</a></li>
+              <li><a href="#/venuelayout">Layout & Pricing</a></li>
+              <li><a href="#/bookvenue">Book Venue</a></li>
+            </ul>
+          </mdb-col>
+          <hr class="clearfix w-100 d-md-none" />
+          <mdb-col md="2">
+            <h5 class="text-uppercase mb-4 mt-3 font-weight-bold">Links</h5>
+            <ul class="list-unstyled">
+              <li><a href="#/admin">Admin Login</a></li>
+            </ul>
+          </mdb-col>
+        </mdb-row>
       </mdb-container>
-    </div>
-  </mdb-footer>
-  <!-- Footer -->
+      <br />
+      <hr />
+      <div class="text-center py-3">
+        <ul class="list-unstyled list-inline mb-0">
+          <li class="list-inline-item">
+            <h5 class="mb-1">Reserve an event today</h5>
+          </li>
+          <li class="list-inline-item"><a href="#/bookvenue" class="btn btn-primary btn-rounded">Book an event!</a></li>
+        </ul>
+      </div>
+      <hr />
+      <div class="text-center">
+        <ul class="list-unstyled list-inline">
+          <br />
+          <li class="list-inline-item"><a class="btn-floating btn-lg btn-fb mx-1"><i class="fab fa-facebook"> </i></a></li>
+          <li class="list-inline-item"><a class="btn-floating btn-lg btn-tw mx-1"><i class="fab fa-twitter"> </i></a></li>
+        </ul>
+      </div>
+      <br />
+      <div class="footer-copyright text-center py-3">
+        <mdb-container fluid>
+          &copy; 2019 Copyright: <a href="#/"> Regal Events Center </a>
+        </mdb-container>
+      </div>
+    </mdb-footer>
+    <!-- Footer -->
   </div>
 </template>
 
--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -2,7 +2,9 @@
   <div class="hello">
     <head>
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta charset="utf-8">
+      <!-- <meta name="viewport" content="width=device-width, initial-scale=1"> -->
+      <meta name="viewport" content="width=device-width">
     </head>
     <body class="front">
       <!-- Navbar -->
