89af9a9ad4231cb4a806a53a6849b4d17c63399a
Francisco Franco <fhfranco32@gmail.com>
2019-11-02 02:41:24 -0400

    renamed admin login page

---
+++ src/components/AdminLogin.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <NavigationBar></NavigationBar>
+    <h1> Admin Login </h1>
+    <b-container>
+
+    </b-container>
+    <Footer> </Footer>
+  </div>
+</template>
+
+<script>
+import NavigationBar from './NavigationBar'
+import Footer from './Footer'
+
+export default {
+  name: 'AdminLogin',
+  data () {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  props: {
+    msg: String
+  },
+  components: {
+    NavigationBar,
+    Footer
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
