4fa62656bbf77b2292895ad0b07874fe0a371ded
Francisco Franco <fhfranco32@gmail.com>
2019-12-03 16:35:05 -0500

    dist

--- src/App.vue
+++ src/App.vue
@@ -20,6 +20,7 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 0px;
-  background-color: lightblue;
+  /* background-color: lightblue; */
+  background-image: url("grad.png");
 }
 </style>
